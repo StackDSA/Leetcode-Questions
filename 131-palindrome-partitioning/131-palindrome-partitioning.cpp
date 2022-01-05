a7c6fb86ec2653c54994db628b3ba4bc0e92a786
Ishan <68941034+StackDSA@users.noreply.github.com>
2022-01-05 23:50:53 +0530

    Time: 219 ms (22.71%), Space: 49.3 MB (79.56%) - LeetHub

---
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -0,0 +1,35 @@
+//Another unstudied topic Q. I can't do it as of now. LC is really behind my back to study unstudied topics. Will start doing questions on it. For now just copying someone else's code for the sake of streak.
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> result;
+        vector<string> path;
+        partition(s, 0, path, result);//dfs calls
+        return result;
+    }
+private: 
+    //DFS steps
+    void partition(string& s, int start, vector<string>& path, vector<vector<string>>& result) {
+        int n = s.length();
+        if (start == n) {
+            result.push_back(path);
+        } else {
+            for (int i = start; i < n; i++) {
+                if (isPalindrome(s, start, i)) {
+                    path.push_back(s.substr(start, i - start + 1));
+                    partition(s, i + 1, path, result);
+                    path.pop_back();
+                }
+            }
+        }
+    }
+    //helper function to safe check whether a substr is palindrome or not
+    bool isPalindrome(string& s, int l, int r) {
+        while (l < r) {
+            if (s[l++] != s[r--]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
