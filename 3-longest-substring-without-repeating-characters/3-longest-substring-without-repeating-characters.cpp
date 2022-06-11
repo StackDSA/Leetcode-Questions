a334980d7fe0ff71964514bff0c0feff512fbfcc
Ishan <68941034+StackDSA@users.noreply.github.com>
2022-06-11 13:31:36 +0530

    Time: 3 ms (98.61%), Space: 8.4 MB (59.31%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,29 +1,40 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        if(s.length()<=1) return s.length();
-        int l=0,r=0, n=s.length(),maxLength=0;
-        vector<int>v(256,0);
+        vector<int>v(256,-1); // We will store the location of the last index where the repeated char is found.
+        int n=s.length(), l=0, r=0, len=0;
         while(r<n){
-            v[s[r]]++;
-            if(r==n-1){
-                if(v[s[r]]>1){
-                    r--;
-                }
-                maxLength=max(maxLength, (r-l+1));
-                return maxLength;
-            }  
-            else if(r<n){
-                if(v[s[r]]>1){
-                maxLength=max(maxLength, (r-l));
-                while(v[s[r]]>1){
-                    v[s[l]]--;
-                    l++;
-                }
-                }
-            }
+            if(v[s[r]]!=-1) l=max(v[s[r]]+1, l);
+            v[s[r]]=r;
+            len=max(len, r-l+1);
             r++;
         }
-        return maxLength;
+        return len;
     }
 };
+
+//This is a TC=O(2*n) and SC=O(n) solution. 
+        // if(s.length()<=1) return s.length();
+        // int l=0,r=0, n=s.length(),maxLength=0;
+        // vector<int>v(256,0);
+        // while(r<n){
+        //     v[s[r]]++;
+        //     if(r==n-1){
+        //         if(v[s[r]]>1){
+        //             r--;
+        //         }
+        //         maxLength=max(maxLength, (r-l+1));
+        //         return maxLength;
+        //     }  
+        //     else if(r<n){
+        //         if(v[s[r]]>1){
+        //         maxLength=max(maxLength, (r-l));
+        //         while(v[s[r]]>1){
+        //             v[s[l]]--;
+        //             l++;
+        //         }
+        //         }
+        //     }
+        //     r++;
+        // }
+        // return maxLength;
