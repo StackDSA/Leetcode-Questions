6710828d7b00dc0171ad39f3bee202dfce054bad
Ishan <68941034+StackDSA@users.noreply.github.com>
2022-07-28 15:01:57 +0530

    Time: 12 ms (60.97%), Space: 7.5 MB (89.67%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -9,6 +9,7 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
+        // Followed solution from a YT video. Implemented on my own. Intuition is key. After we detect a cycle, we increment the ace and slow pointers till they meet. When they meet, we return the slow/ ace pointer. Proof is in the video. 
         if(!head||!head->next) return NULL;
         ListNode* slow=head;
         ListNode* fast=head;
