74106cbd0b931e7b1b342bc9c933e6e578176c03
Ishan <68941034+StackDSA@users.noreply.github.com>
2022-07-31 20:36:25 +0530

    Time: 8 ms (69.45%), Space: 13.6 MB (96.06%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -1,15 +1,17 @@
 class Solution {
+    //Struggled a bit. Didn't catch the idea of "starting at index 0/1" completely. To understand it clearly, read this- https://leetcode.com/problems/min-cost-climbing-stairs/discuss/476388/4-ways-or-Step-by-step-from-Recursion-greater-top-down-DP-greater-bottom-up-DP-greater-fine-tuning/831741
+    //Below is a Space O(1) solution.
 public:
     int minCostClimbingStairs(vector<int>& cost) {
         if(cost.size()==2) return min(cost[0],cost[1]);
         int n=cost.size();
-        int temp[n+1];
+        int curr;
         int first=cost[0];
         int second=cost[1];
         for(int i=2;i<n;i++){
-            temp[i]=cost[i]+min(first,second);
+            curr=cost[i]+min(first,second);
             first=second;
-            second=temp[i];
+            second=curr;
         }
         return min(first,second);
     }
