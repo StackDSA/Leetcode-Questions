77bce8559a257c26d917179e08a4a6f9c3eea1bd
Ishan <68941034+StackDSA@users.noreply.github.com>
2022-07-31 09:09:43 +0530

    Time: 612 ms (66.67%), Space: 149.9 MB (97.60%) - LeetHub

---
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.cpp
@@ -0,0 +1,31 @@
+class NumArray {
+public:
+    vector<int>arr;
+    int sum=0;
+    NumArray(vector<int>& nums) {
+        arr = nums;
+        sum = accumulate(arr.begin(), arr.end(), 0);
+    }
+    
+    void update(int index, int val) {
+        sum -= arr[index];
+        arr[index] = val;
+        sum += val;
+    }
+    
+    int sumRange(int left, int right) {
+        int  res = sum;
+        for(int i = 0; i < left; ++i)
+            res -= arr[i];
+        for(int i = right+1; i < arr.size(); ++i)
+            res -= arr[i];
+        return res;
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * obj->update(index,val);
+ * int param_2 = obj->sumRange(left,right);
+ */
